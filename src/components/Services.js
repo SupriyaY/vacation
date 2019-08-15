819f6e41436c5d8138731fda9c0ac4f96306c53c
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-08-15 19:48:55 -0400

    mapping through services object

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react'
-import Title from './Title'
+import React, { Component } from 'react';
+import Title from './Title';
+import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from "react-icons/fa";
+
 
 export default class Services extends Component {
+    state ={
+        services:[
+            {
+                icon: <FaCocktail/>,
+                title: "Free Cocktails",
+                info:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, iusto!"
+
+            },
+            {
+                icon: <FaHiking/>,
+                title: "Endless Hiking",
+                info:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, iusto!"
+
+            },
+            {
+                icon: <FaShuttleVan/>,
+                title: "Free shuttle",
+                info:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, iusto!"
+
+            },
+            {
+                icon: <FaBeer/>,
+                title: "Strongest Beer",
+                info:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore, iusto!"
+
+            }
+        ]
+    };
     render() {
         return (
-            <div>
+            <section className ="services">
                 <Title title="services"/>
-            </div>
-        )
+                <div className="service-center">
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className="service">
+                        </article>
+                    })}
+                    </div>
+            </section>
+
+        );
     }
 }
 
